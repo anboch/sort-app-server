8e9cba5f7f4e36060c67ed3cb79eb47ae36cc1e2
Andrew Bochka <79652909+anboch@users.noreply.github.com>
2022-05-23 04:52:01 +0300

    create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MaterialModule } from './material/material.module';
+import { BinModule } from './bin/bin.module';
+import { TypeModule } from './type/type.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, MaterialModule, BinModule, TypeModule],
   controllers: [AppController],
   providers: [AppService],
 })
