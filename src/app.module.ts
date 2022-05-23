62b79a326c197608b9aa505c4852c15025113f2d
Andrew Bochka <79652909+anboch@users.noreply.github.com>
2022-05-23 18:23:04 +0300

    feat(user): create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { MaterialModule } from './material/material.module';
 import { BinModule } from './bin/bin.module';
 import { TypeModule } from './type/type.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, MaterialModule, BinModule, TypeModule],
+  imports: [AuthModule, MaterialModule, BinModule, TypeModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
