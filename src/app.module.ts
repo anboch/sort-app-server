f1a4046e83318012c811bc13518437bf3006cdf1
Andrew Bochka <79652909+anboch@users.noreply.github.com>
2022-05-25 01:24:52 +0300

    feat: install and configurate mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,24 @@ import { MaterialModule } from './material/material.module';
 import { BinModule } from './bin/bin.module';
 import { TypeModule } from './type/type.module';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, MaterialModule, BinModule, TypeModule, UserModule],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: getMongoConfig,
+			inject: [ConfigService],
+		}),
+		AuthModule,
+		MaterialModule,
+		BinModule,
+		TypeModule,
+		UserModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,20 @@
+import { ConfigService } from '@nestjs/config';
+import { MongooseModuleFactoryOptions } from '@nestjs/mongoose';
+
+export const getMongoConfig = async (
+	configService: ConfigService
+): Promise<MongooseModuleFactoryOptions> => ({
+	uri: getMongoURI(configService),
+});
+
+const getMongoURI = (configService: ConfigService): string =>
+	'mongodb://' +
+	configService.get('MONGO_LOGIN') +
+	':' +
+	configService.get('MONGO_PASSWORD') +
+	'@' +
+	configService.get('MONGO_HOST') +
+	':' +
+	configService.get('MONGO_PORT') +
+	'/' +
+	configService.get('MONGO_AUTHDATABASE');
