63fd0df410a4896e3e9efc207ec62957a31c3aea
Andrew Bochka <79652909+anboch@users.noreply.github.com>
2022-05-27 04:25:16 +0300

    add collectionNames var

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -18,3 +18,13 @@ const getMongoURI = (configService: ConfigService): string =>
 	configService.get('MONGO_PORT') +
 	'/' +
 	configService.get('MONGO_AUTHDATABASE');
+
+export const collectionNames = {
+	MATERIAL: 'materials',
+	TYPE: 'types',
+	CLUSTER: 'clusters',
+	USER: 'users',
+	RECYCLE_POINT: 'recyclePoints',
+	BIN: 'bins',
+	CATEGORY: 'categories',
+};
